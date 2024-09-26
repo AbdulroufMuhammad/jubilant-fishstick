ebf0e3f5436c8363f1accaff4b3a04129d2584d9
Abdulrouf Muhammad <109133424+AbdulroufMuhammad@users.noreply.github.com>
2024-09-26 04:15:10 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI, UploadFile, File
+import cloudinary
+import cloudinary.uploader
+from dotenv import load_dotenv
+import os
+
+app = FastAPI()
+
+load_dotenv(dotenv_path="upload/.env")
+
+cloudinary.config(
+    cloud_name="dl581kkmn",
+    api_key=os.getenv('API_KEY'),
+    api_secret=os.getenv('API_SECRET'),
+    secure=True
+)
+
+@app.get("/")
+def Home():
+    return {"message": "Welcome"}
+
+@app.post("/upload_file")
+async def upload_file(file: UploadFile = File(...)):
+    try:
+        result = cloudinary.uploader.upload(file.file, public_id="profile")
+        return {"secure_url": result["secure_url"]}
+    except Exception as e:
+        return {"error": str(e)}
